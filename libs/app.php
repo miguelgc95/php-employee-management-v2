ee720ffff76c84bc96d6b1717e708eef39e47c9d
miguelgc95 <miguelgc95@gmail.com>
2021-02-27 12:31:57 +0100

    Error controller and view

--- libs/app.php
+++ libs/app.php
@@ -1,25 +1,30 @@
 <?php
 
-
-
 class App
 {
     public function __construct()
     {
-        $url = isset($_GET['url']) ? $_GET['url']: null;
+        $url = isset($_GET['url']) ? $_GET['url'] : null;
         $url = rtrim($url, '/');
         $url = explode('/', $url);
 
         // cuando se ingresa sin definir controlador
-        if(empty($url[0])){
-            require_once
+        if (empty($url[0])) {
             $archivoController = 'controllers/main.php';
             require_once $archivoController;
             $controller = new Main();
             $controller->loadModel('main');
             $controller->render();
             return false;
+        } else {
+            $archivoController = 'controllers/' . $url[0] . '.php';
+            if (file_exists($archivoController)) {
+                echo "ok";
+            } else {
+                $archivoController = 'controllers/errors.php';
+                require_once $archivoController;
+                new Errors();//igual no hace falta definir la variable no?
+            }
         }
-        $archivoController = 'controllers/' . $url[0] . '.php';
     }
 }
