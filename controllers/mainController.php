aca8351529b4d3219cc2f9a2ca138710a67cc9cc
joserra-15 <joseramonsr3@hotmail.com>
2021-03-03 15:15:19 +0100

    Session finish

--- controllers/mainController.php
+++ controllers/mainController.php
@@ -14,7 +14,7 @@ class Main extends Controller
         if ($user = $this->model->getUser($_POST["email"], $_POST["password"])) {
             $this->saveSession($user);
             header('Location: ' . URL . 'dashboard');
-        }else{
+        } else {
             $this->view->message = 'Incorrect e-mail or password';
             $this->view->render('main/index');
         }
@@ -27,20 +27,24 @@ class Main extends Controller
 
     private function saveSession($user)
     {
-        session_start(); //App instanciado
         $_SESSION['id'] = $user['id'];
         $_SESSION['email'] = $user['email'];
         $_SESSION['name'] = $user['name'];
         $_SESSION['init'] = time();
-        $_SESSION['life'] = 600;
+        $_SESSION['life'] = 6;
     }
 
     function logout()
     {
-        session_start(); //instanciado en App
         session_destroy();
         $this->view->message = "Logout correctly";
         $this->view->render('main/index');
+    }
 
+    function logoutByTime()
+    {
+        session_destroy();
+        $this->view->message = "Your session has expired";
+        $this->view->render('main/index');
     }
 }
